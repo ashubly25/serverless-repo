eb5e4702f9b37da49de2de5ba7040e393ea251b6
Ash <ashutosh2564@gmail.com>
2022-06-09 04:54:52 +0530

    Update late.js

--- late.js
+++ late.js
@@ -2,3 +2,9 @@
 function running_late(date){
     if(new Date(date).getHours() >= 22){
         return "It is late!";
+    } 
+    else {
+        return "It is still early!";
+    }
+}
+exports.running_late = running_late;
